488143b597781d491e6c31f5f668f5c687920637
dennis774 <rodriguezdm.774.stud@cdd.edu.ph>
2024-10-27 10:46:52 +0800

    basic login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChartDataController;
 
@@ -7,4 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('dashboard', ChartDataController::class);
+Route::resource('dashboard-main', ChartDataController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
