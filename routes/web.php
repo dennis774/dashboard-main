b2f665bbfa8156daaeda25c3767fdd8a8085b1aa
dennis774 <rodriguezdm.774.stud@cdd.edu.ph>
2024-10-27 14:11:39 +0800

    remove basic login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChartDataController;
 
@@ -9,6 +8,3 @@ Route::get('/', function () {
 });
 
 Route::resource('dashboard-main', ChartDataController::class);
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
